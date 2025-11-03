b3df411571c4396c3e9a37584cb450d0a7c1980e
HaiTrieu186 <pnhtrieu186@gmail.com>
2025-11-03 22:29:20 +0700

    Sua lai middleware check token (them kiem tra user ton tai)

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,22 +1,35 @@
 const jwt= require("jsonwebtoken")
+const model=require("../models/index.model")
 
-module.exports.verifyToken = (req,res, next )=> {
+module.exports.verifyToken = async  (req,res, next )=> {
     
     try {
         const token = req.header("Authorization") && req.header("Authorization").replace("Bearer ","");
 
         if (!token)
             return res.status(401).json({
-                message:"Vui lòng đăng nhập để có token"
+                success:false,
+                message:"Vui lòng đăng nhập "
             })
        
        try {
             const data= jwt.verify(token,process.env.JWT_SECRET_KEY);
             req.user=data;
+
+            const user = await model.User.findOne({
+                where:{id: data.id}
+            })
+
+            if (!user)
+                return res.status(403).json({
+                    success:false,
+                    message:"User khong hop le!"
+            })
        } catch (error) {
             //console.log(" lỗi verify token:", error.message);
             //console.log(" Full error:", error); // Xem chi tiết
             return res.status(401).json({
+                success:false,
                 message:"Token không hợp lệ",
                 error:error.message
             });
@@ -27,6 +40,7 @@ module.exports.verifyToken = (req,res, next )=> {
 
     } catch (error) {
         return res.status(500).json({
+            success:false,
             message:"Đã có lỗi xảy ra khi xác thực token",
             error:error.message
         });   
